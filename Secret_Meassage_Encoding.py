85870b3769b523a2fa96ec152b786db22137448c
__niikett__ <154818816+niikett@users.noreply.github.com>
2024-03-12 22:43:45 +0530

    Create Secret_Message_Encoding.py

---
+++ Secret_Meassage_Encoding.py
@@ -0,0 +1,37 @@
+def encoded_message(message, shift) :
+
+  new_message = ""  
+
+  for n in range(0, len(message)):
+    if shift >= 0:
+      new_message_ord = ord(message[n]) + shift
+      if int(new_message_ord) > 122:
+        new_message_ord -= 26
+        new_message_chr = chr(new_message_ord)
+        new_message += new_message_chr  
+      else:
+        new_message_chr = chr(new_message_ord)
+        new_message += new_message_chr  
+    else:    
+      new_message_ord = ord(message[n]) + shift
+      if int(new_message_ord) < 97:
+        new_message_ord += 26
+        new_message_chr = chr(new_message_ord)
+        new_message += new_message_chr   
+      else:
+        new_message_chr = chr(new_message_ord)
+        new_message += new_message_chr  
+
+  print(new_message)
+
+while True:
+  message = input("Enter the message: ")
+
+  shift = int(input("Enter PIN: "))
+
+  encoded_message(message, shift)
+
+  choice = input("Want to encode more messages (YES or NO) : ").lower()
+
+  if choice == "no":
+    exit()
